123decae8870117529b329c3baf983c9c3e3e491
Rahemul Islam <rahemul.islam@northsouth.edu>
2022-09-25 05:23:59 +0600

    nvm installed

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var http = require('http');
+var http = require("http");
 
 var server = http.createServer(function (req, res) {
-    res.end("hello Word")
-})
+  res.end("hello Word");
+});
 
+setTimeout(() => {
+  console.log("runed");
+}, 5000);
 server.listen(5050);
-console.log("runed");
