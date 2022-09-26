dace2411d26b399149911615031bec19ea44d593
Rahemul Islam <rahemul.islam@northsouth.edu>
2022-09-27 03:21:45 +0600

    http module tested

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-var http = require("http");
+const http = require("http");
 
-var server = http.createServer(function (req, res) {
-  res.end("hello Word");
+const server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    res.write(req.url);
+    res.end();
+  } else if (req.url === "/gotit") {
+    res.write("gotta logtta");
+    res.end();
+  } else {
+    res.write(req.url);
+    res.end();
+  }
 });
 
-setTimeout(() => {
-  console.log("runed");
-}, 5000);
-server.listen(5050);
+// server.listen(3000);
+// console.log("running");
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("Hello programmers!");
+//     res.write("How are you doing?");
+//     res.end();
+//   } else if (req.url === "/about") {
+//     res.write("This is about us page");
+//     res.end();
+//   } else {
+//     res.write("Not found");
+//     res.end();
+//   }
+// });
+
+server.listen(3000);
+
+console.log("listening on port 3000");
